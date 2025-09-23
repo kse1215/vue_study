66350f70b49b60db85fe07e8c58c34594053d0f5
kse1215 <dev.kseoeun@gmail.com>
2025-09-23 10:49:54 +0900

    와이어프레임 구조잡기 연습

---
+++ vue_ex01/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import FooterWrap from './components/FooterWrap.vue';
+import HeaderTop from './components/HeaderTop.vue';
+import Visual from './components/Visual.vue';
+
+</script>
+
+<template>
+  <h1>app vue</h1>
+  <HeaderTop/>
+  <Visual/>
+  <FooterWrap/>
+</template>
+
+<style scoped></style>
---
+++ vue_ex01/src/components/FooterWrap.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>Footer 입니다</h2>
+</template>
+<script setup></script>
+<style scoped>
+h2 {
+  color: lightsteelblue;
+}
+</style>
